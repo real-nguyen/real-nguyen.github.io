8b695924e25f6474e34acb37ddfc8cc3d6d9f3ea
Real Nguyen <real.nguyen@hotmail.com>
2020-07-15 13:01:23 -0400

    Edited font glyphs, added support for 12h mode

--- src/App.js
+++ src/App.js
@@ -4,30 +4,13 @@ import Clock from './components/Clock';
 
 export class App extends React.Component {
   state = {
-    time: new Date().toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit' })
+    timeMode: '24'
   };
-
-  tick() {
-    this.setState({
-      time: new Date().toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit' })
-    });
-  }
-
-  // Analogous to Angular's onInit
-  componentDidMount() {
-    // Calls tick every second
-    this.intervalId = setInterval(() => this.tick(), 1000);
-  }
-
-  // Analogous to Angular's onDestroy
-  componentWillUnmount() {
-    clearInterval(this.intervalId);
-  }
-
+  
   render() {
     return (
       <div className="App">
-        <Clock time={this.state.time}/>
+        <Clock timeMode={this.state.timeMode}/>
       </div>
     );
   }
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import './Clock.css';
+import PropTypes from 'prop-types';
 
 export class Clock extends React.Component {
+  setTime() {
+    const { timeMode } = this.props;
+    let time = new Date().toLocaleTimeString([], { 
+      hour12: timeMode === '12', 
+      hour: '2-digit', 
+      minute: '2-digit' 
+    });
+    if (timeMode === '12') {
+      // Remove AM/PM indicator
+      return time.split(' ')[0];
+    }
+    return time;
+  }
+
+  state = {
+    time: this.setTime()
+  };
+  
+  tick() {
+    this.setState({ time: this.setTime() });
+  }
+
+  // Analogous to Angular's onInit
+  componentDidMount() {
+    // Calls tick every second
+    this.intervalId = setInterval(() => this.tick(), 1000);
+  }
+
+  // Analogous to Angular's onDestroy
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
   render () {
     return (
       <div className="Clock">
-        <h1>{this.props.time}</h1>
+        <h1>{this.state.time}</h1>
+        <div className="hourMode">
+
+        </div>
       </div>
     )
   }
+}
+
+Clock.propTypes = {
+  timeMode: PropTypes.object.isRequired
 }
 
 export default Clock;
